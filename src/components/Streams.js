900c4ee9a641b896b6b940467073488b8b3fef5a
Tian Qiu <tian.qiu142@gmail.com>
2020-10-30 09:31:38 -0700

    completed project with shorter code

--- src/components/Streams.js
+++ src/components/Streams.js
@@ -2,257 +2,42 @@ import React from 'react';
 import { Card, Image, Container, Row, Col } from 'react-bootstrap';
 import faker from 'faker';
 
+const renderCards = (numOFCards) => {
+  let container = [];
+  for (let i = 0; i < numOFCards; i++) {
+    container.push(
+      <Col key={faker.random.word()}>
+        <Card>
+          <Card.Img
+            variant="top"
+            src={faker.image.image()}
+            // style={{ width: 100 }}
+          />
+          <Card.Body>
+            <Card.Title>
+              <Image
+                src={faker.image.avatar()}
+                roundedCircle
+                style={{ width: 35, marginRight: 10 }}
+              />
+              {faker.internet.userName()}
+            </Card.Title>
+            <Card.Text>{faker.random.words()}</Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  }
+  return container;
+};
+
 const Streams = () => {
   return (
     <Container fluid>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img
-              variant="top"
-              src={faker.image.image()}
-              // style={{ width: 100 }}
-            />
-            <Card.Body>
-              <Card.Title>
-                <Image
-                  src={faker.image.avatar()}
-                  roundedCircle
-                  style={{ width: 35, marginRight: 10 }}
-                />
-                {faker.internet.userName()}
-              </Card.Title>
-              <Card.Text>{faker.random.words()}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+      <Row>{renderCards(4)}</Row>
+      <Row>{renderCards(4)}</Row>
+      <Row>{renderCards(4)}</Row>
+      <Row>{renderCards(4)}</Row>
     </Container>
   );
 };
